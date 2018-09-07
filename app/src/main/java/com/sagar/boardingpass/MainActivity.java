c32a1b0560ce893c991dfcee6ca82e6e37fdfa3e
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-09-07 23:26:41 +0530

    displayPassInfo() - set the data to all textViews

--- app/src/main/java/com/sagar/boardingpass/MainActivity.java
+++ app/src/main/java/com/sagar/boardingpass/MainActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 
 import com.sagar.boardingpass.databinding.ActivityMainBinding;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding mBinding;
@@ -18,6 +22,38 @@ public class MainActivity extends AppCompatActivity {
 
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+    }
+
+
+    private void displayBoardingPassInfo(BoardingPassInfo info) {
+
+        mBinding.textViewPassengerName.setText(info.passengerName);
+        mBinding.textViewOriginAirport.setText(info.originCode);
+        mBinding.textViewFlightCode.setText(info.flightCode);
+        mBinding.textViewDestinationAirport.setText(info.destCode);
+
+        SimpleDateFormat formatter = new SimpleDateFormat(getString(R.string.timeFormat), Locale.getDefault());
+        String boardingTime = formatter.format(info.boardingTime);
+        String departureTime = formatter.format(info.departureTime);
+        String arrivalTime = formatter.format(info.arrivalTime);
+
+        mBinding.textViewBoardingTime.setText(boardingTime);
+        mBinding.textViewDepartureTime.setText(departureTime);
+        mBinding.textViewArrivalTime.setText(arrivalTime);
+
+        long totalMinutesUntilBoarding = info.getMinutesUntilBoarding();
+        long hoursUntilBoarding = TimeUnit.MINUTES.toHours(totalMinutesUntilBoarding);
+        long minutesLessHoursUntilBoarding =
+                totalMinutesUntilBoarding - TimeUnit.HOURS.toMinutes(hoursUntilBoarding);
+
+        String hoursAndMinutesUntilBoarding = getString(R.string.countDownFormat,
+                hoursUntilBoarding,
+                minutesLessHoursUntilBoarding);
+
+        mBinding.textViewBoardingInCountdown.setText(hoursAndMinutesUntilBoarding);
+        mBinding.textViewTerminal.setText(info.departureTerminal);
+        mBinding.textViewGate.setText(info.departureGate);
+        mBinding.textViewSeat.setText(info.seatNumber);
     }
 
 
