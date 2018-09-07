0adbb6ceac76295fd1f4ce161c63bd51d01c6cd5
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-09-07 22:18:40 +0530

    Binding instance has been set

--- app/src/main/java/com/sagar/boardingpass/MainActivity.java
+++ app/src/main/java/com/sagar/boardingpass/MainActivity.java
@@ -1,13 +1,25 @@
 package com.sagar.boardingpass;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.sagar.boardingpass.databinding.ActivityMainBinding;
+
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding mBinding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
     }
+
+
+    // END
 }
